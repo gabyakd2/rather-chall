26abeb8d386802e241cde3c1cbc9e2d1056cf732
gabyakd2 <gabrielsal00@hotmail.com>
2024-02-19 20:00:54 -0300

    fix: nft's card ready

--- app/components/Card/InformationCard.tsx
+++ app/components/Card/InformationCard.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { Newsreader } from "next/font/google";
+
+const newsreader = Newsreader({ subsets: ["latin"] });
 
 function InformationCard() {
   return (
-    <div className="w-1/4 flex flex-col justify-center">
-        <p className="text-5xl">
-          Buy luxurious items coupled with utility NFTs
+    <div className=" flex flex-col justify-center">
+      <p className={`${newsreader.className} text-6xl leading-none font-normal`}>
+        Buy luxurious <br /> items coupled with <br /> utility NFTs
+      </p>
+      <p className="text-xl w-96 mt-4">
+        The marketplace that merges physical luxury and digital security.
+      </p>
+      <button className="mt-12 flex justify-start relative group w-72">
+        <p className="mb-4 transition-transform transform-gpu group-hover:translate-x-2">
+          Get Started
         </p>
-        <p className="text-xl w-96 mt-16">
-          The marketplace that merges physical luxury and digital security.
-        </p>
-      <button className="mt-20 flex justify-start">Get Started</button>
+        <div className="absolute bottom-0 left-0 w-full h-0.5 bg-white transition-all transform origin-left hover:bg-gray-500"></div>
+      </button>
     </div>
   );
 }
--- app/components/Card/MediaCard.tsx
+++ app/components/Card/MediaCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function MediaCard() {
   return (
     <div>
